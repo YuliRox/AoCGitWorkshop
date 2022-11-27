de75af106bde6cbaed7487cd4fca484e9e010d01
Ben Teggemann <ben@teggemann.de>
2022-11-27 17:55:27 +0100

    working version for day 1 part 1

--- main.cpp
+++ main.cpp
@@ -28,5 +28,16 @@ int main(int argc, char *argv[])
 
     cout << "Read " << numbers.size() << " numbers!\n";
 
+    int cntr = 0;
+
+    for (int i = 0 ; i < numbers.size() - 1 ; i++){
+        if ( numbers[i] < numbers[i + 1] ){
+            cntr ++;
+        }
+    }
+
+    cout << "die Loeseung ist " << cntr << "\n";
+
+
     return 0;
 }
