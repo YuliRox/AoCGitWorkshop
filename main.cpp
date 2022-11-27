b45bd5df7eb685be1006736c0294902f40d34041
Xemrox <xemrox@user.noreply.github.com>
2022-11-27 16:31:10 +0100

    added input pipelining with sample data

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 
 #include <iostream>
+#include <vector>
+#include <limits>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!\n";
+    vector<int> numbers;
+    while(cin.peek() != EOF) {
+        int parsedNumber = 0;
+        cin >> parsedNumber;
+
+        // detect invalid input
+        if(cin.fail()) {
+            cout << "something went wrong parsing input\n";
+            // reset error bits on cin
+            cin.clear();
+            // skip invalid input up until newline
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            // skip adding to vector
+            continue;
+        }
+
+        numbers.push_back(parsedNumber);
+    }
+
+    cout << "Read " << numbers.size() << " numbers!\n";
+
     return 0;
 }
