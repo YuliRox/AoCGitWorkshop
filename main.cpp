42699aafac6853288d0967c8c8fc9c9e362f62cc
Ben Teggemann <ben@teggemann.de>
2022-11-27 18:09:38 +0100

    Working version for day one part one and two

--- main.cpp
+++ main.cpp
@@ -8,12 +8,15 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     vector<int> numbers;
-    while(cin.peek() != EOF) {
+
+    while (cin.peek() != EOF)
+    {
         int parsedNumber = 0;
         cin >> parsedNumber;
 
         // detect invalid input
-        if(cin.fail()) {
+        if (cin.fail())
+        {
             cout << "something went wrong parsing input\n";
             // reset error bits on cin
             cin.clear();
@@ -27,17 +30,33 @@ int main(int argc, char *argv[])
     }
 
     cout << "Read " << numbers.size() << " numbers!\n";
+    cout << "AoCGitWorkshop 2021 day 1 part 1\n";
 
     int cntr = 0;
 
-    for (int i = 0 ; i < numbers.size() - 1 ; i++){
-        if ( numbers[i] < numbers[i + 1] ){
-            cntr ++;
+    for (int i = 0; i < numbers.size() - 1; i++)
+    {
+        if (numbers[i] < numbers[i + 1])
+        {
+            cntr++;
         }
     }
 
-    cout << "die Loeseung ist " << cntr << "\n";
+    cout << "the solution for part one is " << cntr << "\n";
 
+    cout << "AoCGitWorkshop 2021 day 1 part 2\n";
+
+    cntr = 0;
+
+    for (int i = 0; i < numbers.size() - 3; i++)
+    {
+        if (numbers[i] + numbers[i + 1] + numbers[i + 2] < numbers[i + 1] + numbers[i + 2] + numbers[i + 3])
+        {
+            cntr++;
+        }
+    }
+
+    cout << "the solution for part two is " << cntr << "\n";
 
     return 0;
 }
