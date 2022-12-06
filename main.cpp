e3a5c72c55edd5827621136acec36a2b4224f864
Ben Teggemann <ben@teggemann.de>
2022-12-06 13:45:01 +0100

    AoC22 day 2. Faulty, works on test data, but fails on real imput.

--- main.cpp
+++ main.cpp
@@ -2,106 +2,115 @@
 #include <iostream>
 #include <vector>
 #include <limits>
-//#include <string>
 
 using namespace std;
 
+int score(char elve, char me)
+{
+    int shape;
+    const int win = 6;
+    const int draw = 3;
+    const int loose = 0;
+
+    switch (elve)
+    {
+    case 'A': // Rock
+        shape = 1;
+        switch (me)
+        {
+        case 'A':
+            return shape + draw;
+        case 'B':
+            return shape + win;
+        case 'C':
+            return shape + loose;
+        }
+    case 'B':  // Paper
+        shape = 2;
+        switch (me)
+        {
+        case 'A':
+            return shape + loose;
+        case 'B':
+            return shape + draw;
+        case 'C':
+            return shape + win;
+        }
+    case 'C': // Scissors
+        shape = 3;
+        switch (me)
+        {
+        case 'A':
+            return shape + win;
+        case 'B':
+            return shape + loose;
+        case 'C':
+            return shape + draw;
+        }
+    default:
+        int e = elve;
+        int m = me;
+        cout << "unreckognised shape! " << elve << e << me << m << "\n";
+        return -1;
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    cout << "AoCGitWorkshop 2022 day 1 part 1 & 2\n";
+    cout << "AoCGitWorkshop 2022 day 2 part 1\n";
 
-    vector<int> elves;
-    long calsPerElv = 0;
-    int nlCnt = 0;
-    int parsedNumber = 0;
-    bool goOnReading = true;
+    char elve, me;
+    char cnvShape[] = {'A', 'B', 'C', 'A', 'B', 'C'};
 
-    while (goOnReading)
+    int readLines = 0;
+    long posScores[] = {0, 0, 0};
+
+    while (cin.peek() != EOF)
     {
         char c = cin.get();
 
         switch (c)
         {
-            case EOF:
-                goOnReading = false;
-            case 10: // LF
-                if ( nlCnt == 0) // Number finisched. Now add!
-                {
-                    cout << "Cals read " << parsedNumber << "\n";        
-                    calsPerElv += parsedNumber;
-                    parsedNumber = 0;
-                    nlCnt++;
-                }
-                else if ( nlCnt > 0) // switch elve
-                {
-                    cout << "Total cals on elve " << calsPerElv << "\n";        
-                    elves.push_back(calsPerElv);
-                    calsPerElv = 0;
-                    nlCnt = 0;
-                }
+            case 'A':
+            case 'B':
+            case 'C':
+                elve = c;
                 break;
-            case '0': // numbers
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-            c -= 48; // offset in ascii table
-                parsedNumber = parsedNumber * 10 + c;
-                nlCnt = 0;
+            case 'X':
+            case 'Y':
+            case 'Z':
+                me = c;
+                posScores['X' - 'X'] += score(elve, cnvShape[me - 'X' + 3]);
+                posScores['Y' - 'X'] += score(elve, cnvShape[me - 'Y' + 3]);
+                posScores['Z' - 'X'] += score(elve, cnvShape[me - 'Z' + 3]);
+                readLines++;
                 break;
         }
     }
 
-    cout << "\nThere are " << elves.size() << " on the expedition.\n";
+    cout << "\n*** Answer part 1 ***\n\n";
+    cout << "Lines read: " << readLines <<"\n";
+    cout << "your options are:\n";
+    cout << "Option | X        | Y        | Z        | Score\n";
+    cout << "    #1 | Rock     | Paper    | Scissors | " << posScores[0] << "\n";
+    cout << "    #2 | Paper    | Scissors | Rock     | " << posScores[1] << "\n";
+    cout << "    #3 | Scissors | Rock     | Paper    | " << posScores[2] << "\n";
 
-    int maxCals = 0;
-    int powerElvesNumber = 0;
-
-    for (int i = 0; i < elves.size(); i++)
+    cout << "\nThe highest scoring option is option ";
+    int maxScore = 0;
+    int bestOption = 0;
+    for (int i = 0; i < sizeof(posScores) / sizeof(posScores[0]) ; i++)
     {
-        if (elves[i] > maxCals)
+        if ( posScores[i] > maxScore)
         {
-            maxCals = elves[i];
-            powerElvesNumber = i + 1;
+            maxScore = posScores[i];
+            bestOption = i + 1;
         }
     }
-
-    cout << "\n* Answer part 1 *\n";
-    cout << "The number " << powerElvesNumber << " elve carryies the most calories. \nHe carryes " << maxCals << " cals.\n";
-
-    // On each run the highest number ist moved to the first position
-    // In consequende each run can omit one more element at the beginning
-    const int mRuns = 3;
-    int sortTmp;
-
-    for ( int j = 0 ; j < mRuns ; j++ )
-    {
-        for (int i = elves.size() - 1; i > j ; i--)
-        {
-            if (elves[i - 1] < elves[i])
-            {
-                sortTmp = elves[i - 1];
-                elves[i - 1] = elves[i];
-                elves[i] = sortTmp;
-            }
-        }
-        
-    }
-
-    int topElvesCals = 0;
-
-    for (int i = 0; i < mRuns; i++)
-    {
-        topElvesCals += elves[i];
-    }
+    cout << maxScore << ".\n\n";
     
-    cout << "\n** Answer part 2 **\n";
-    cout << "The total calories carried by the top " << mRuns << " elves are " << topElvesCals << " cals.\n";
+
+//    cout << "\n**** Answer part 2 ****\n";
 
     return 0;
 }
